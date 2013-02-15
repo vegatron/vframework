d9a7b72a8e35d99da5c373ad80b3b2ec8a9add06
Ahmed Reeh <A@Reeh.ly>
2013-02-15 01:15:17 +0100

    new start

---
+++ system/language/arabic/scaffolding_lang.php
@@ -0,0 +1,40 @@
+﻿<?php
+
+/*
+ 
+* Contributors: 
+ 
+* Emad Elsaid <blazeeboy@gmail.com>
+ 
+* Aimad Jait <aimadphp@gmail.com>
+ 
+*/
+
+
+$lang['scaff_view_records'] = 'عرض السجلات';
+
+$lang['scaff_create_record'] = 'انشاء سجل جديد';
+
+$lang['scaff_add'] = 'اضافة بيانات';
+
+$lang['scaff_view'] = 'عرض البيانات';
+
+$lang['scaff_edit'] = 'تحرير';
+
+$lang['scaff_delete'] = 'الغاء';
+
+$lang['scaff_view_all'] = 'عرض الكل';
+
+$lang['scaff_yes'] = 'نعم';
+
+$lang['scaff_no'] = 'لا';
+
+$lang['scaff_no_data'] = 'لا توجد بيانات فى هذا الجدول بعد.';
+
+$lang['scaff_del_confirm'] = 'هل تريد فعلآ الغاء هذا السطر';
+
+
+
+/* End of file scaffolding_lang.php */
+
+/* Location: ./system/language/arabic/scaffolding_lang.php */
