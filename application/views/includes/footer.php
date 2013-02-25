6e3937a9f1f39b1a10c580c1600096cbdcf58a00
Ahmed Reeh <A@Reeh.ly>
2013-02-25 01:27:56 +0100

    footer - constant

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -16,7 +16,7 @@
       <div class="container">
         <p class="pull-right"><?php //<a href="#">Back to top</a> ; ?></p>
         <p>
-			<?php echo APP_TITLE_LONG; ?>, &copy; <?php echo APP_AUTHOR; ?> 2013, All rights reserved.
+			<?php echo APP_TITLE_LONG; ?>, &copy; <?php echo AUTHOR_NAME; ?> 2013, All rights reserved.
         </p>
       </div>
     </footer>
